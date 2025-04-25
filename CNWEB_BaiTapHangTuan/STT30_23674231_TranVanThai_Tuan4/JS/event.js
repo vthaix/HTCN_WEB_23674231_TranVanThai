5c30ba380d205075541dc33096128098d1579c1a
vthaix <cheat3049@gmail.com>
2025-04-25 17:07:35 +0700

    Delete CNWEB_BaiTapHangTuan directory

--- CNWEB_BaiTapHangTuan/STT30_23674231_TranVanThai_Tuan4/JS/event.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("btn-Facebook").addEventListener("click", function(){   
-    alert("Chuyển hướng tới....")
-
-    setTimeout(function() {
-        window.location.href = "https://www.facebook.com/r.php?entry_point=login&locale=vi_VN";}, 300);
-}) 
