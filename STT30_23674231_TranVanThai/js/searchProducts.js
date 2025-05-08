e66be8cfbd14e18aa5e87633d1cc6768451f0018
vthaix <cheat3049@gmail.com>
2025-05-08 13:56:35 +0700

    searchProducts.js

--- STT30_23674231_TranVanThai/js/searchProducts.js
+++ STT30_23674231_TranVanThai/js/searchProducts.js
@@ -10,8 +10,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const suggestions = document.getElementById('suggestions');
 
     searchBox.addEventListener('input', function(event) {
+        // Lấy giá trị người dùng nhập vào và chuyển thành chữ thường
         const query = event.target.value.toLowerCase();
-
+        // Xóa nội dung cũ của danh sách gợi ý
         suggestions.innerHTML = '';
 
         if (!query) return;
@@ -21,9 +22,10 @@ document.addEventListener("DOMContentLoaded", function() {
         );
 
         filteredProducts.forEach(product => {
+            // Tạo thẻ `<li>` cho từng sản phẩm
             const li = document.createElement('li');
             li.className = 'list-group-item d-flex align-items-center';
-
+            // Tạo thẻ `<img>` để hiển thị hình ảnh sản phẩm
             const img = document.createElement('img');
             img.src = product.imageUrl;
             img.alt = product.name;
@@ -32,17 +34,17 @@ document.addEventListener("DOMContentLoaded", function() {
             img.style.objectFit = 'cover';
             img.style.marginRight = '10px';
             img.style.borderRadius = '8px';
-
+            // Tạo thẻ `<span>` để hiển thị tên sản phẩm
             const span = document.createElement('span');
             span.textContent = product.name;
-
+            // Thêm ảnh và tên sản phẩm vào thẻ `<li>`
             li.appendChild(img);
             li.appendChild(span);
 
             li.addEventListener('click', function() {
                 window.location.href = `chitiet_sp.html?type=${product.type}&id=${product.id}`;
             });
-
+            // Them thẻ `<li>` vào danh sách gợi ý
             suggestions.appendChild(li);
         });
     });
